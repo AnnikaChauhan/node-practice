d8bc2247b135f3982b45120d4f29c4dbdac84256
Annika Chauhan <annikac0711@gmail.com>
2020-05-02 16:04:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('this is the homepage');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('this is the contact page');
+});
+
+app.get('/profile/:name', (req, res) => {
+    res.send('You requested to see a profile with the name of ' + req.params.name);
+});
+
+app.listen(3000);
