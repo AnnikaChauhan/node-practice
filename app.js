f4906b9c15fb0621b95433512357fd3fa7c399f3
Annika Chauhan <annikac0711@gmail.com>
2020-05-03 15:08:43 +0100

    implemented templates using EJS template engine

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('this is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', (req, res) => {
-    res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', (req, res) => {
-    res.send('You requested to see a profile with the name of ' + req.params.name);
+    const data = {age: 29, job: 'software developer', hobbies: ['code code code', 'swimming', 'painting']}
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
