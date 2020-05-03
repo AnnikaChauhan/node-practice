8c92e4dc9366560617bdc6c15332c927c2293753
Annika Chauhan <annikac0711@gmail.com>
2020-05-03 16:41:21 +0100

    made assets file and produced middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
 const app = express();
+
 app.set('view engine', 'ejs');
 
+//this is the middleware
+app.use('/assets', express.static('assets'));
+
+//anything between the function start and the res is middleware
 app.get('/', (req, res) => {
     res.render('index');
 });
