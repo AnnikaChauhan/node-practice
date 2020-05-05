1db2892faba9f494a09250718ca9665ff56910d2
Annika Chauhan <annikac0711@gmail.com>
2020-05-05 11:17:46 +0100

    POST request added and successful post page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 
@@ -14,6 +17,12 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
     // console.log(req.query);
     res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    //nodemailer will let you send the post request on to a database
+    res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', (req, res) => {
